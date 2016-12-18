88123c0213c93037b458a8ee5ed2d5eb65c55d3d
Taolanoz <maxence.renglet@gmail.com>
2016-12-18 19:09:10 +0100

    Update NetflixCheck.py

--- NetflixCheck.py
+++ NetflixCheck.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python
 # coding=utf-8
-#MassLoginChecker v.0.2
-"""
-Copyright (C) 2016, Alan Levy
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-__author__ = "Alan Levy"
-__email__ = "alevy@cintainfinita.com"
-__license__ = "GPL"
-
-##########################################SETEAR RUTA DE ARCHIVO DE USUARIOS/CLAVES#######################################
 
 archivo = "credenciales.txt"
 print "--------------------------[Netflix Checker by Taolano]--------------------------"
@@ -26,7 +7,6 @@ try:
 	import requests
 	import sys, os
 	import os.path
-	#Libreria para trabajar y buscar dentro de un html
 	from BeautifulSoup import BeautifulSoup as Soup
 except ImportError:
 	print "Needed dependencies: 'requests', 'BeautifulSoup'"
@@ -62,4 +42,3 @@ for lineas in lectura:
 	email=lineas.split(":")[0]
 	password=lineas.split(":")[1]
 	checkPassword(email,password)
-
